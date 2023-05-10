0a6d7ec2e5f3521d8d3ffec1b98233de91f167eb
François ILLIEN <francois.illien@etu.univ-nantes.fr>
2023-05-10 17:09:19 +0200

    Add support to YAML scenario file and add a example scenario

--- main.cpp
+++ main.cpp
@@ -1,28 +1,51 @@
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <iterator>
 #include <memory>
+#include <vector>
 
 #include "engine.hpp"
 #include "event.hpp"
 #include "scheduler.hpp"
 #include "task.hpp"
 #include "tracer.hpp"
+#include "yaml-cpp/node/node.h"
+#include "yaml-cpp/yaml.h"
 
-int main() {
-        using namespace std;
 
-        constexpr size_t NB_PROCESSOR = 1;
+int main(const int argc, const char** argv) {
         constexpr double START_TIME = 0;
 
-        auto sim = make_shared<engine>(NB_PROCESSOR);
+	using namespace std;
+
+	if (argc != 2) {
+		std::cerr << "No input scenario\n";
+		return EXIT_FAILURE;
+	}
+
+	// Open the scenario
+	YAML::Node config = YAML::LoadFile(argv[1]);
+
+        auto sim = make_shared<engine>(config["cores"].as<int>());
         auto sched = make_shared<scheduler>();
         sim->set_scheduler(sched);
 
-        // Execute task 1 a time 0, for a duration of 5 units
-        auto t1 = make_shared<task>(1, 4, 0.5);
-        auto t2 = make_shared<task>(2, 6, 0.5);
-        sim->add_event({types::JOB_ARRIVAL, t1, 5}, START_TIME);
-        sim->add_event({types::JOB_ARRIVAL, t2, 8}, START_TIME);
+	std::vector<std::shared_ptr<task>> tasks {config["tasks"].size()};
+	// Create tasks and job arrival events
+	for (auto node: config["tasks"]) {
+		auto new_task = make_shared<task>(node["id"].as<int>(),
+						  node["period"].as<double>(),
+						  node["utilization"].as<double>());
 
+		for (auto job: node["jobs"]) {
+			sim->add_event({types::JOB_ARRIVAL, new_task,
+					job["duration"].as<double>()},
+				job["arrival"].as<double>());
+		}
+		tasks.push_back(std::move(new_task));
+	}
+	
         // Simulate the system (job set + platform) with the chosen scheduler
         sim->simulation();
 
