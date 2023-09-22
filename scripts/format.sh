1d72b7632f214a99a41af65c14a50268116c6664
François ILLIEN <francois.illien@etu.univ-nantes.fr>
2023-09-22 10:03:57 +0200

    Add sched_mono class to the formatting script

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,6 +5,7 @@ clang-format -i src/main.cpp src/engine.hpp src/engine.cpp \
 	     src/processor.hpp src/processor.cpp \
 	     src/task.hpp \
 	     src/scheduler.hpp src/scheduler.cpp \
+	     src/sched_mono.hpp src/sched_mono.cpp \
 	     src/server.cpp src/server.hpp \
 	     src/event.cpp src/event.hpp \
 	     src/tracer.hpp src/tracer.cpp
