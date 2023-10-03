6422de9bfd0965b46b0e6860d9627699ab60fc9e
François Illien <francois@illien.org>
2023-10-03 13:07:18 +0200

    Set the starting processor number indice at 1

--- src/plateform.cpp
+++ src/plateform.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 plateform::plateform(const std::weak_ptr<engine> sim, const size_t nb_proc) : entity(sim) {
-        for (size_t i = 0; i < nb_proc; ++i) {
+        for (size_t i = 1; i <= nb_proc; ++i) {
                 auto new_proc = std::make_shared<processor>(sim, i);
                 processors.push_back(std::move(new_proc));
         }
