8bb0bc61823622b21fe1c4cad685b727cf5579ba
François Illien <francois@illien.org>
2024-01-02 12:36:47 +0100

    Add task id to the SVG

--- schedview/src/gantt/svg.cpp
+++ schedview/src/gantt/svg.cpp
@@ -9,6 +9,7 @@
 namespace {
 constexpr double AXIS_HEIGHT{64};
 constexpr double TIME_UNIT{100};
+constexpr double OFFSET_X{30};
 const double EVENT_HEIGHT{40};
 
 constexpr auto TAU_SYM{"&#120591;"};
@@ -35,8 +36,8 @@ std::string style{"<style>"
 auto operator<<(std::ostream& out, const outputs::gantt::arrival& evt) -> std::ostream&
 {
         const double OFFSET_Y{AXIS_HEIGHT * static_cast<double>(evt.index)};
-        out << "<line class='event' x1='" << TIME_UNIT * evt.timestamp << "' x2='"
-            << TIME_UNIT * evt.timestamp;
+        out << "<line class='event' x1='" << OFFSET_X + TIME_UNIT * evt.timestamp << "' x2='"
+            << OFFSET_X + TIME_UNIT * evt.timestamp;
         out << "' y1='" << OFFSET_Y << "' y2='" << OFFSET_Y - EVENT_HEIGHT;
         out << "'><title>arrival: " << evt.timestamp << "</title>";
         out << "</line>";
@@ -46,8 +47,8 @@ auto operator<<(std::ostream& out, const outputs::gantt::arrival& evt) -> std::o
 auto operator<<(std::ostream& out, const outputs::gantt::deadline& evt) -> std::ostream&
 {
         const double OFFSET_Y{AXIS_HEIGHT * static_cast<double>(evt.index)};
-        out << "<line class='event' x1='" << TIME_UNIT * evt.timestamp << "' x2='"
-            << TIME_UNIT * evt.timestamp;
+        out << "<line class='event' x1='" << OFFSET_X + TIME_UNIT * evt.timestamp << "' x2='"
+            << OFFSET_X + TIME_UNIT * evt.timestamp;
         out << "' y1='" << OFFSET_Y - EVENT_HEIGHT << "' y2='" << OFFSET_Y;
         out << "'><title>deadline: " << evt.timestamp << "</title>";
         out << "</line>";
@@ -59,7 +60,7 @@ auto operator<<(std::ostream& out, const outputs::gantt::execution& evt) -> std:
         constexpr double TASK_HEIGHT_MAX{30};
         const double TASK_OFFSET_Y{static_cast<double>(evt.index - 1) * AXIS_HEIGHT + 33};
         const double DURATION{evt.stop - evt.start};
-        out << "<rect class='task' x='" << TIME_UNIT * evt.start << "' y='" << TASK_OFFSET_Y;
+        out << "<rect class='task' x='" << OFFSET_X + TIME_UNIT * evt.start << "' y='" << TASK_OFFSET_Y;
         out << "' width='" << TIME_UNIT * DURATION << "' height='" << TASK_HEIGHT_MAX;
         out << "' fill='" << outputs::gantt::get_color_hex(evt.cpu) << "'>";
         out << "<title>";
@@ -76,7 +77,7 @@ auto operator<<(std::ostream& out, const outputs::gantt::active_non_cont& evt) -
         constexpr double TASK_HEIGHT_MAX{30};
         const double TASK_OFFSET_Y{static_cast<double>(evt.index - 1) * AXIS_HEIGHT + 33};
         const double DURATION{evt.stop - evt.start};
-        out << "<rect class='anc' x='" << TIME_UNIT * evt.start << "' y='" << TASK_OFFSET_Y;
+        out << "<rect class='anc' x='" << OFFSET_X + TIME_UNIT * evt.start << "' y='" << TASK_OFFSET_Y;
         out << "' width='" << TIME_UNIT * DURATION << "' height='" << TASK_HEIGHT_MAX;
         out << "'>";
         out << "<title>";
@@ -106,15 +107,17 @@ template <typename T> auto operator<<(std::ostream& out, const std::vector<T>& v
 auto outputs::gantt::svg::draw(const outputs::gantt::gantt& chart) -> std::string
 {
         std::stringstream out;
-        out << "<svg viewBox='0 0 " << chart.duration * TIME_UNIT;
+        out << "<svg viewBox='0 0 " << OFFSET_X + chart.duration * TIME_UNIT;
         out << " " << static_cast<double>(chart.nb_axis) * AXIS_HEIGHT
             << "' xmlns='http://www.w3.org/2000/svg'>\n";
         out << defs << style << '\n';
         for (std::size_t i = 1; i <= chart.nb_axis; ++i) {
                 auto axis = static_cast<double>(i);
-                out << "<line x1='0' y1='" << AXIS_HEIGHT * axis;
-                out << "' x2='" << chart.duration * TIME_UNIT;
-                out << "' y2='" << AXIS_HEIGHT * axis << "' stroke='black' stroke-width='1'/>\n";
+		auto BASELINE {AXIS_HEIGHT * axis};
+		out << "<text x='0' y='" << BASELINE - (AXIS_HEIGHT/4) << "'>" << TAU_SYM << i << "</text>";
+                out << "<line x1='" << OFFSET_X << "' y1='" << BASELINE;
+                out << "' x2='" << OFFSET_X + chart.duration * TIME_UNIT;
+                out << "' y2='" << BASELINE << "' stroke='black' stroke-width='1'/>\n";
         }
         out << chart.commands;
         out << "</svg>\n";
