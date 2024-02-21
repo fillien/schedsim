5883007f5399cbfe7f387cd6498ac208d00f9ef4
François Illien <francois@illien.org>
2024-02-21 17:38:54 +0100

    Overload out stream for gantt::finished events

--- schedview/src/gantt/rtsched.cpp
+++ schedview/src/gantt/rtsched.cpp
@@ -31,6 +31,11 @@ auto operator<<(std::ostream& out, const outputs::gantt::active_non_cont& evt) -
                    << "}";
 }
 
+auto operator<<(std::ostream& out, const outputs::gantt::finished& evt) -> std::ostream&
+{
+        return out;
+}
+
 auto operator<<(std::ostream& out, const outputs::gantt::command& cmd) -> std::ostream&
 {
         std::visit([&](auto&& cmd) { out << cmd; }, cmd);
