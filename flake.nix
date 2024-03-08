3e9d1620174163c0c9f707f1ace3ec9c2d3d2cb6
François Illien <francois@illien.org>
2024-03-08 13:01:25 +0100

    Format flake and add bash as dependency

--- flake.nix
+++ flake.nix
@@ -7,26 +7,33 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
-    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
-  ] (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in {
-  formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-    devShells.default = pkgs.mkShell rec {
-      name = "schesim";
-      packages = with pkgs; [
-        cmake
-        doxygen
-        graphviz
-        ninja
-        gtest
-        gdb
-	shellcheck
-      ];
-    };
+    "x86_64-linux"
+    "i686-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+  ]
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell rec {
+          name = "schesim";
+          packages = with pkgs; [
+            bash
+            cmake
+            doxygen
+            graphviz
+            ninja
+            gtest
+            gdb
+            shellcheck
+          ];
+        };
 
-    packages.default = pkgs.callPackage ./package.nix {};
-  });
+        packages.default = pkgs.callPackage ./package.nix { };
+      });
 }
--- test.sh
+++ test.sh
@@ -1,56 +1,82 @@
-#!/bin/sh +x
+#!/usr/bin/env bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <commit_hash_version1> <commit_hash_version2>"
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <commit_hash_version1> <commit_hash_version2> <nb_test_tasks>"
     exit 1
 fi
 
-VERSION1=$1
-VERSION2=$2
+COMMIT_BACKUP=$(git rev-parse HEAD)
+
+# Call exit with the exit code in arg
+function finish() {
+    git checkout "$COMMIT_BACKUP"
+    exit "$1"
+}
+
+# Test if current branch is the one speficied in arg 1
+function ami_commit() {
+    if [ "$1" != "$(git rev-parse HEAD)" ]; then
+	echo "The script is on the wrong commit"
+	finish 1	
+    fi
+}
+
+VERSION1=$(git rev-parse "$1")
+VERSION2=$(git rev-parse "$2")
+NB_TASKS=$3
 
 TESTS_DIR=./build_sce
 PLATFORM_FILE=$TESTS_DIR/platform.json
 SCENARIO_DIR=$TESTS_DIR/scenarios
 LOGS_DIR=$TESTS_DIR/logs
-mkdir "$TESTS_DIR"
+
+if [[ -e "${TESTS_DIR}" ]]; then
+    rm -rf ${TESTS_DIR}
+fi
+mkdir "${TESTS_DIR}"
 
 # Build the first version
-git checkout "$VERSION1"
-cmake -S . -B "build_$VERSION1" -G Ninja -DCMAKE_BUILD_TYPE=Release
-cmake --build "build_$VERSION1" 
+git checkout "${VERSION1}"
+ami_commit "${VERSION1}"
+cmake -S . -B "build_${VERSION1}" -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build "build_${VERSION1}" 
 
 # Build the second version
-git checkout "$VERSION2"
-cmake -S . -B "build_$VERSION2" -G Ninja -DCMAKE_BUILD_TYPE=Release
-cmake --build "build_$VERSION2" 
+git checkout "${VERSION2}"
+ami_commit "${VERSION2}"
+cmake -S . -B "build_${VERSION2}" -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build "build_${VERSION2}" 
 
 # Generate platform and scenarios
-./"build_$VERSION1"/schedgen/schedgen platform --output "$PLATFORM_FILE" --cores 2 --freq 1
-mkdir "${SCENARIO_DIR}"
-for i in {1..5}; do
-    NB_TASKS=5
+./"build_${VERSION1}"/schedgen/schedgen platform --output "${PLATFORM_FILE}" --cores 2 --freq 1
+mkdir -p "${SCENARIO_DIR}"
+for i in {1..20}; do
     NB_JOBS=5
     TOTALU=1.5
-    ./"build_$VERSION1"/schedgen/schedgen taskset -o "${SCENARIO_DIR}/${i}.json" --tasks "$NB_TASKS" --jobs "$NB_JOBS" --totalu "$TOTALU" --success 1
+    ./"build_${VERSION1}"/schedgen/schedgen taskset -o "${SCENARIO_DIR}/${i}.json" --tasks "${NB_TASKS}" --jobs "${NB_JOBS}" --totalu "${TOTALU}" --success 1
 done
 
-ls -l "$TESTS_DIR"
+ls -l "${TESTS_DIR}"
 
 # Run the CLI program for both versions and compare the outputs
 mkdir -p "${LOGS_DIR}/${VERSION1}" "${LOGS_DIR}/${VERSION2}"
-for file in "$SCENARIO_DIR"/*.json
+for file in "${SCENARIO_DIR}"/*.json
 do
-    echo $file
-    ./"build_$VERSION1"/schedsim/schedsim --platform "$PLATFORM_FILE" --scenario "$file" --policy "grub" --output "$LOGS_DIR/$VERSION1/$(basename "$file")"
-    ./"build_$VERSION2"/schedsim/schedsim --platform "$PLATFORM_FILE" --scenario "$file" --policy "grub" --output "$LOGS_DIR/$VERSION2/$(basename "$file")"
-    diff <(jq --sort-keys . "$LOGS_DIR/$VERSION1/$(basename "$file")") <(jq --sort-keys . "$LOGS_DIR/$VERSION2/$(basename "$file")")
+    echo "${file}"
+    ./"build_${VERSION1}"/schedsim/schedsim --platform "${PLATFORM_FILE}" --scenario "${file}" --policy "grub" --output "${LOGS_DIR}/${VERSION1}/$(basename "$file")"
+    ./"build_${VERSION2}"/schedsim/schedsim --platform "${PLATFORM_FILE}" --scenario "${file}" --policy "grub" --output "${LOGS_DIR}/$VERSION2/$(basename "$file")"
+    diff <(jq --sort-keys . "${LOGS_DIR}/${VERSION1}/$(basename "$file")") <(jq --sort-keys . "${LOGS_DIR}/${VERSION2}/$(basename "$file")")
 
-    ./"build_$VERSION1"/schedsim/schedsim --platform "$PLATFORM_FILE" --scenario "$file" --policy "pa" --output "$LOGS_DIR/$VERSION1/$(basename "$file")"
-    ./"build_$VERSION2"/schedsim/schedsim --platform "$PLATFORM_FILE" --scenario "$file" --policy "pa" --output "$LOGS_DIR/$VERSION2/$(basename "$file")"
-    diff <(jq --sort-keys . "$LOGS_DIR/$VERSION1/$(basename "$file")") <(jq --sort-keys . "$LOGS_DIR/$VERSION2/$(basename "$file")")
+    ./"build_${VERSION1}"/schedsim/schedsim --platform "${PLATFORM_FILE}" --scenario "${file}" --policy "pa" --output "${LOGS_DIR}/${VERSION1}/$(basename "$file")"
+    ./"build_${VERSION2}"/schedsim/schedsim --platform "${PLATFORM_FILE}" --scenario "${file}" --policy "pa" --output "${LOGS_DIR}/${VERSION2}/$(basename "$file")"
+    diff <(jq --sort-keys . "${LOGS_DIR}/${VERSION1}/$(basename "$file")") <(jq --sort-keys . "${LOGS_DIR}/${VERSION2}/$(basename "$file")")
 done
 
 # Clean up (optional)
 #rm -rf "build_$VERSION1" "build_$VERSION2" output_version1.txt output_version2.txt
 
-git checkout "$VERSION1"
+finish 0
