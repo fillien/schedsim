4db6552621ec6e427d58129e53224d8b0be07142
François Illien <francois@illien.org>
2024-07-21 19:51:58 +0200

    Move to inherit nix package from the system

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Schedsim is a C++ scheduling simulator";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
