f72a4fac0be32eb60a4af3150c92322a000c2fa8
François Illien <francois@illien.org>
2024-08-28 16:33:38 +0200

    Remove unused scripts

--- scripts/generate-trace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-~/.local/bin/barectf generate --code-dir=src --headers-dir=src --include-dir=src --metadata-dir=trace src/config.yaml
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cmake --build build -t launch && cmake --build build -t viewer && ./build/src/launch "$1" && ./build/viewer/viewer -o mydessin.tex out.json
