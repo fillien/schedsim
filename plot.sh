e6d8a7ab394fd10460e689ff3a0bcc62794e2872
François Illien <francois@illien.org>
2024-10-20 17:12:40 +0200

    Remove global scripts

--- plot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in $(seq 1 9)
-do
-    ./scripts/energy-consumption-vs-utilization.py data_umax0_$i
-    mv energy_util.png energy_util_umax0_$i.png
-    mv data-energy.csv energy_umax0_$i.csv
-done
