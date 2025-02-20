7059336ef256f41623b85e4692bbf6f00159ac06
François Illien <francois@illien.org>
2025-02-20 10:21:19 +0100

    refactor update_platform of FFA/CSF timer schedulers

--- schedsim/include/schedulers/csf_timer.hpp
+++ schedsim/include/schedulers/csf_timer.hpp
@@ -11,6 +11,8 @@ class CsfTimer : public DpmDvfs {
         void update_platform() override;
 
       private:
+        void manage_dpm_timer(auto next_active_procs);
+        void manage_dvfs_timer(auto next_freq);
         std::shared_ptr<Timer> timer_dvfs_cooldown = std::make_shared<Timer>(sim(), [this]() {
                 if (chip()->freq() != chip()->ceil_to_mode(freq_after_cooldown)) {
                         for (const auto& proc : chip()->processors()) {
--- schedsim/src/schedulers/csf_timer.cpp
+++ schedsim/src/schedulers/csf_timer.cpp
@@ -13,6 +13,57 @@ CsfTimer::CsfTimer(const std::weak_ptr<Engine>& sim) : DpmDvfs(sim)
         }
 }
 
+void CsfTimer::manage_dpm_timer(const auto next_active_procs)
+{
+        int diff_dpm = next_active_procs - nb_active_procs();
+        if (diff_dpm > 0) {
+                for (int i = 0; i < diff_dpm; ++i) {
+                        activate_next_core();
+                }
+        }
+        else if (diff_dpm < 0) {
+                // Compute the cover
+                int couverture = diff_dpm + timers_dpm_cooldown.size();
+
+                assert(couverture != 0);
+                if (couverture > 0) {
+                        // Cancel youngest timers
+                        std::ranges::sort(
+                            timers_dpm_cooldown,
+                            [](const std::shared_ptr<Timer>& first,
+                               const std::shared_ptr<Timer>& second) {
+                                    return first->deadline() < second->deadline();
+                            });
+
+                        for (int i = 0; i < diff_dpm; ++i) {
+                                assert(!timers_dpm_cooldown.empty());
+                                auto tmp = timers_dpm_cooldown.begin();
+                                tmp->get()->cancel();
+                                timers_dpm_cooldown.erase(tmp);
+                        }
+                }
+                else if (couverture < 0) {
+                        for (int i = couverture; i > 0; --i) {
+                                auto timer_dpm = std::make_shared<Timer>(
+                                    sim(), [this]() { activate_next_core(); });
+                                timer_dpm->set(DPM_COOLDOWN);
+                                timers_dpm_cooldown.push_back(std::move(timer_dpm));
+                        }
+                }
+        }
+}
+
+void CsfTimer::manage_dvfs_timer(const auto next_freq)
+{
+        if (chip()->freq() != chip()->ceil_to_mode(next_freq)) {
+                freq_after_cooldown = chip()->ceil_to_mode(next_freq);
+                if (!timer_dvfs_cooldown->is_active()) { timer_dvfs_cooldown->set(DVFS_COOLDOWN); }
+        }
+        else if (timer_dvfs_cooldown->is_active()) {
+                timer_dvfs_cooldown->cancel();
+        }
+}
+
 void CsfTimer::update_platform()
 {
         const double total_util{active_bandwidth()};
@@ -50,51 +101,8 @@ void CsfTimer::update_platform()
         next_active_procs = clamp(next_active_procs);
         assert(next_active_procs >= 1 && next_active_procs <= max_procs);
 
-        // Manage DPM timers
-        int diff_dpm = next_active_procs - nb_active_procs();
-        if (diff_dpm > 0) {
-                for (int i = 0; i < diff_dpm; ++i) {
-                        activate_next_core();
-                }
-        }
-        else if (diff_dpm < 0) {
-                // Compute the cover
-                int couverture = diff_dpm + timers_dpm_cooldown.size();
-
-                assert(couverture != 0);
-                if (couverture > 0) {
-                        // Cancel youngest timers
-                        std::ranges::sort(
-                            timers_dpm_cooldown,
-                            [](const std::shared_ptr<Timer>& a, const std::shared_ptr<Timer>& b) {
-                                    return a->deadline() < b->deadline();
-                            });
-
-                        for (int i = 0; i < diff_dpm; ++i) {
-                                assert(!timers_dpm_cooldown.empty());
-                                auto tmp = timers_dpm_cooldown.begin();
-                                tmp->get()->cancel();
-                                timers_dpm_cooldown.erase(tmp);
-                        }
-                }
-                else if (couverture < 0) {
-                        for (int i = couverture; i > 0; --i) {
-                                auto timer_dpm = std::make_shared<Timer>(
-                                    sim(), [this]() { activate_next_core(); });
-                                timer_dpm->set(DPM_COOLDOWN);
-                                timers_dpm_cooldown.push_back(std::move(timer_dpm));
-                        }
-                }
-        }
-
-        // Manage DVFS timer
-        if (chip()->freq() != chip()->ceil_to_mode(next_freq)) {
-                freq_after_cooldown = chip()->ceil_to_mode(next_freq);
-                if (!timer_dvfs_cooldown->is_active()) { timer_dvfs_cooldown->set(DVFS_COOLDOWN); }
-        }
-        else if (timer_dvfs_cooldown->is_active()) {
-                timer_dvfs_cooldown->cancel();
-        }
+        manage_dpm_timer(next_active_procs);
+        manage_dvfs_timer(next_freq);
 }
 
 } // namespace scheds
