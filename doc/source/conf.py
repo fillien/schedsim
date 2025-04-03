ef29de279678d5365621eff7d991fd855f50c120
François Illien <francois@illien.org>
2025-04-03 21:34:31 +0200

    add a sphinx/breathe config

---
+++ doc/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'schedsim'
+copyright = '2025, François ILLIEN'
+author = 'François ILLIEN'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['breathe']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+breathe_default_project = "schedsim"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_material'
+html_static_path = ['_static']
+
+breathe_projects = {"schedsim": "../xml"}
---
+++ doc/source/index.rst
@@ -0,0 +1,17 @@
+.. schedsim documentation master file, created by
+   sphinx-quickstart on Thu Apr  3 16:44:13 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+schedsim documentation
+======================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Docs
+====
+
+.. doxygenfile:: platform.hpp
+.. doxygenfile:: platform.cpp
