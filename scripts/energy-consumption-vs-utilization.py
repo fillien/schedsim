197662678c312977214d665733711d2617a56909
François Illien <francois@illien.org>
2025-04-06 22:45:37 +0200

    add a argument to choose the platform model

--- scripts/energy-consumption-vs-utilization.py
+++ scripts/energy-consumption-vs-utilization.py
@@ -8,13 +8,13 @@ from concurrent.futures import ThreadPoolExecutor
 from io import StringIO
 
 SCHEDVIEW = "./build/schedview/schedview"
-PLATFORM = "./platforms/exynos5422LITTLE.json"
 
 def main():
     if len(sys.argv) <= 1:
         print("Please pass the tasksets as an argument")
         return
 
+    PLATFORM = sys.argv[2]
     logs = sys.argv[1]
 
     utilizations = []
@@ -31,7 +31,7 @@ def main():
     }
 
     def process_scheduler(scheduler, directory):
-        return run_scenario(SCHEDVIEW, os.path.join(log_paths[scheduler], directory))["power"].mean()
+        return run_scenario(SCHEDVIEW, os.path.join(log_paths[scheduler], directory), PLATFORM)["power"].mean()
 
     current_util = 0.1
 
@@ -67,9 +67,9 @@ def main():
     results.to_csv(sys.argv[2], index=False, sep=" ")
 
 
-def run_scenario(schedview, logs_path):
+def run_scenario(schedview, logs_path, platform):
     output = subprocess.run(
-        [SCHEDVIEW, "--platform", PLATFORM, "--directory", logs_path, "--energy", "--duration", "--index"],
+        [SCHEDVIEW, "--platform", platform, "--directory", logs_path, "--energy", "--duration", "--index"],
         capture_output=True,
         text=True,
         check=True,
